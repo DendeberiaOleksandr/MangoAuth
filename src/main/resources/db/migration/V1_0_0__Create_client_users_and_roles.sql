eac585dfdadc577bf1303414727fa45e9c11e3c4
Serevin21 <pototsky.ilya212@gmail.com>
2024-11-01 15:53:57 +0100

    review

---
+++ src/main/resources/db/migration/V1_0_0__Create_client_users_and_roles.sql
@@ -0,0 +1,28 @@
+CREATE TABLE client (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    apiKey VARCHAR(255),
+    api_secret VARCHAR(255)
+);
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE users_client_role (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    client_id UUID NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE
+);
+
+
+
