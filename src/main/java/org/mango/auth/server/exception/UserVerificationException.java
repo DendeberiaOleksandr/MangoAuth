aded134adc98673bd5408d3e93b067edf8cbecf6
Oleksandr Dendeberia <oleksandr.dendeberia@gmail.com>
2024-11-10 22:15:19 +0100

    DEV-6 moved hard-coded key to final string

--- src/main/java/org/mango/auth/server/exception/UserVerificationException.java
+++ src/main/java/org/mango/auth/server/exception/UserVerificationException.java
@@ -6,11 +6,13 @@ import java.util.Map;
 
 public class UserVerificationException extends ApiException {
 
+    private static final String BAN_TIME_IN_SECONDS = "banTimeInSeconds";
+
     @Getter
     private final long banTimeInSeconds;
 
     public UserVerificationException(String message, String logMessage, String code, Integer errorCode, long banTimeInSeconds) {
-        super(message, logMessage, code, errorCode, Map.of("banTimeInSeconds", banTimeInSeconds));
+        super(message, logMessage, code, errorCode, Map.of(BAN_TIME_IN_SECONDS, banTimeInSeconds));
         this.banTimeInSeconds = banTimeInSeconds;
     }
 
