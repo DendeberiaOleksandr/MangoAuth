383a33a4e688fb82206125d086872d4665ed69f4
Serevin21 <pototsky.ilya212@gmail.com>
2024-11-18 19:27:43 +0100

    DEV-5 Corrected ITBase

--- src/test/java/org/mango/auth/server/integration/ITBase.java
+++ src/test/java/org/mango/auth/server/integration/ITBase.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +16,7 @@ import org.testcontainers.utility.DockerImageName;
 
     @SpringBootTest
     @AutoConfigureMockMvc
+	@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
     @Testcontainers
     public class ITBase  {
 
@@ -26,10 +28,6 @@ import org.testcontainers.utility.DockerImageName;
 	@Container
 	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.1"));
 
-        @ServiceConnection
-        PostgreSQLContainer<?> postgresContainer() {
-            return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-        }
 
 	@DynamicPropertySource
 	static void props(DynamicPropertyRegistry registry) {
